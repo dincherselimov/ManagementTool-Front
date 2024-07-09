da5e77566cd1b5af6e63d7a7affacd1e5297e942
unknown <dinchitoo@gmail.com>
2024-07-09 22:06:43 +0300

    creating-apps

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -3,7 +3,7 @@ import * as types from './typesInterface';
 
 export const login = async (email: string, password: string): Promise<types.LoginResponse> => {
   try {
-    const response = await fetch('https://managementtool-backend.onrender.com/api/login', {
+    const response = await fetch('http://localhost:3000/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
