c3a192b56a04fc053c2be4e48009edffccda4173
dincherselimov <dinchitoo@gmail.com>
2024-07-21 23:25:25 +0300

    renew

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { useAuth } from '../hooks/useAuth';
-import { useState } from 'react';
 
 /**
  * Navbar component
@@ -8,34 +7,25 @@ import { useState } from 'react';
  */
 export default function Navbar() {
   const { isAuthenticated, logout } = useAuth();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
 
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar-brand">
-          <Link href="/">Home</Link>
+          <Link href="/">
+          Home
+          </Link>
         </div>
         <div className="navbar-menu">
-          <div className="navbar-item dropdown" onClick={toggleDropdown}>
-            <span>Apps</span>
-            {dropdownOpen && (
-              <div className="dropdown-menu">
-                <Link className="dropdown-item" href="/apps/planner">Planner</Link>
-                <Link className="dropdown-item" href="/">Inventory</Link>
-                <Link className="dropdown-item" href="/">Document Generator</Link>
-              </div>
-            )}
-          </div>
-          <Link className="navbar-item" href="/profile/myProfile">Profile</Link>
+        <Link className='navbar-item' href="/profile/myProfile">Profile</Link>
           {!isAuthenticated ? (
             <>
-              <Link className="navbar-item" href="/authentication/login">Login</Link>
-              <Link className='navbar-item' href="/authentication/register">Register</Link>
+              <Link className="navbar-item" href="/authentication/login">
+                  Login
+              </Link>
+              <Link className='navbar-item' href="/authentication/register">
+                  Register
+              </Link>
             </>
           ) : (
             <div onClick={logout} className="navbar-item logout-button">Logout</div>
